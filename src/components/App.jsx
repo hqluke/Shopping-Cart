5ef2bde24cda7b091310f3918b22d7e90b7c2bb9
hqluke <hqluke@users.noreply.github.com>
2026-01-02 08:24:24 -0700

    Added images to home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,27 +17,57 @@ function App() {
                 </div>
                 <div className="c">
                     <div className="cc">
-                        <div className="hc">
+                        <div
+                            className="hc"
+                            style={{
+                                "--bg-image":
+                                    "url(/ebca4415b6315474f387e920bb06bb69.jpg)",
+                            }}
+                        >
                             <Link to="/shop/smartphones">
                                 <p>Phones</p>
                             </Link>
                         </div>
-                        <div className="hc">
+                        <div
+                            className="hc"
+                            style={{
+                                "--bg-image":
+                                    "url(/0755dac2159096e80ee08deca7b6755a.jpg)",
+                            }}
+                        >
                             <Link to="/shop/laptops">
                                 <p>Laptops</p>
                             </Link>
                         </div>
-                        <div className="hc">
+                        <div
+                            className="hc"
+                            style={{
+                                "--bg-image":
+                                    "url(/efa4d40b0f49c6be4b3dcced1f6e2276.jpg)",
+                            }}
+                        >
                             <Link to="/shop/fragrances">
                                 <p>Fragrances</p>
                             </Link>
                         </div>
-                        <div className="hc">
+                        <div
+                            className="hc"
+                            style={{
+                                "--bg-image":
+                                    "url(/df5b7166cc792f0751547a0fb2256c81.jpg)",
+                            }}
+                        >
                             <Link to="/shop/mens-watches">
                                 <p>Mens watches</p>
                             </Link>
                         </div>
-                        <div className="hc">
+                        <div
+                            className="hc"
+                            style={{
+                                "--bg-image":
+                                    "url(/screenshot-2025-12-31_01-09-10_flipped.png)",
+                            }}
+                        >
                             <Link to="/shop/womens-watches">
                                 <p>Womens watches</p>
                             </Link>
